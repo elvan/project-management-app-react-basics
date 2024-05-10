7ea0fe4ac86b4e52fae306404771499217ee5564
Elvan Hidayat <elvanhd@gmail.com>
2024-05-10 18:59:05 +0700

    Add NewProject component and enhance layout
    - Integrate NewProject component into the App's main area
    - Implement flexible layout with gap in App for new components
    - Add Input component with configurable props for form elements
    - Design NewProject with Save and Cancel actions and input fields
    - Utilize NewProject to include Title, Description, and Due Date fields

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import NewProject from './components/NewProject.jsx';
 import ProjectsSidebar from './components/ProjectsSidebar.jsx';
 
 function App() {
   return (
-    <main className='h-screen my-8'>
+    <main className='h-screen my-8 flex gap-8'>
       <ProjectsSidebar />
+      <NewProject />
     </main>
   );
 }
