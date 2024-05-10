d40a9736f736a8937b0ad7258bcd993039c1914f
Elvan Hidayat <elvanhd@gmail.com>
2024-05-10 20:43:52 +0700

    Implement project selection and display logic
    - Add handleSelectProject to update selected project ID
    - Introduce SelectedProject component for detail view
    - Enhance ProjectsSidebar with project selection logic
    - Update ProjectsSidebar to highlight selected project
    - Implement conditional rendering based on project state
    - Pass onSelectProject to ProjectsSidebar from App
    - Style selected project to differentiate from others

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,22 @@ import { useState } from 'react';
 import NewProject from './components/NewProject.jsx';
 import NoProjectSelected from './components/NoProjectSelected.jsx';
 import ProjectsSidebar from './components/ProjectsSidebar.jsx';
+import SelectedProject from './components/SelectedProject.jsx';
 
 function App() {
   const [projectsState, setProjectsState] = useState({
     selectedProjectId: undefined,
     projects: [],
   });
+
+  function handleSelectProject(id) {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: id,
+      };
+    });
+  }
 
   function handleStartAddProject() {
     setProjectsState((prevState) => {
@@ -43,7 +53,11 @@ function App() {
     });
   }
 
-  let content;
+  const selectedProject = projectsState.projects.find(
+    (project) => project.id === projectsState.selectedProjectId
+  );
+
+  let content = <SelectedProject project={selectedProject} />;
 
   if (projectsState.selectedProjectId === null) {
     content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />;
@@ -56,6 +70,7 @@ function App() {
       <ProjectsSidebar
         onStartAddProject={handleStartAddProject}
         projects={projectsState.projects}
+        onSelectProject={handleSelectProject}
       />
       {content}
     </main>
