662ad2856b0cf4082b3dbd93c6bb7cc54d0d46d7
Elvan Hidayat <elvanhd@gmail.com>
2024-05-10 20:04:04 +0700

    Implement project addition and refine input handling
    - Add handleAddProject function to manage project creation
    - Utilize useRef and forwardRef for controlled input fields
    - Implement conditional rendering based on project state
    - Enable dynamic project list update in ProjectsSidebar
    - Include form submission through the Save button in NewProject
    - Refactor Input component to support ref forwarding
    - Pass project addition callback to NewProject component

--- src/App.jsx
+++ src/App.jsx
@@ -18,17 +18,36 @@ function App() {
     });
   }
 
+  function handleAddProject(projectData) {
+    setProjectsState((prevState) => {
+      const projectId = Math.random();
+      const newProject = {
+        ...projectData,
+        id: projectId,
+      };
+
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  }
+
   let content;
 
   if (projectsState.selectedProjectId === null) {
-    content = <NewProject />;
+    content = <NewProject onAdd={handleAddProject} />;
   } else if (projectsState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
   }
 
   return (
     <main className='h-screen my-8 flex gap-8'>
-      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectsSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectsState.projects}
+      />
       {content}
     </main>
   );
